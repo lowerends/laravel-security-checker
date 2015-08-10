70ad813f8f32fa1e1aa6837ab8be61ba9cd45308
lowerends <lowerends@gmail.com>
2015-08-10 11:40:37 +0200

    Improved output of security issues

--- src/Console/CheckCommand.php
+++ src/Console/CheckCommand.php
@@ -6,7 +6,7 @@ use SensioLabs\Security\SecurityChecker;
 class CheckCommand extends Command
 {
     protected $name = 'security:check';
-    
+
     protected $description = 'Check your Laravel project for known security issues';
 
     public function __construct()
@@ -20,12 +20,34 @@ class CheckCommand extends Command
 
         $alerts = $checker->check(base_path() . '/composer.lock');
 
-        if (!empty($alerts)) {
-            foreach ($alerts as $alert) {
-                $this->alert('Security issue found: ' . $alert);
+        if (!empty($alerts))
+        {
+            foreach ($alerts as $package => $alert)
+            {
+                $this->error('Security issues found!');
+
+                $this->info('======================');
+
+                $this->info('Package: ' . $package);
+
+                foreach ($alert['advisories'] as $advisory)
+                {
+                    $this->info('Version: ' . $alert['version']);
+
+                    $this->info('Title: ' . $advisory['title']);
+                    
+                    $this->info('Link: ' . $advisory['link']);
+
+                    if($advisory['cve'] != "")
+                    {
+                        $this->info('CVE: ' . $advisory['cve']);
+                    }
+                }
             }
 
-        } else {
+        }
+        else
+        {
             $this->info('No security issues found!');
         }
     }
